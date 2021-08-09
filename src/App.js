6a0c07c3170908887927de6a2d55561fab5f7692
Juan Carlos Maya <juan.maya@coytex.com.co>
2021-08-09 12:28:13 -0500

    Practica de Rutas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 // import Jsx from './components/Jsx';
 // import Lista from './components/Lista';
 // import Formulario from './components/Formulario';
 // import FormHook from './components/FormHook';
 // import Saludo from './components/Saludo';
-import Comentario from './components/Comentario';
+// import Comentario from './components/Comentario';
+import Contacto from './components/Contacto';
+import Nosotros from './components/Nosotros';
+import Inicio from './components/Inicio';
 
 function App() {
-  const sujeto = {
-    nombre: 'Juanito',
-    urlImagen: 'https://via.placeholder.com/64',
-    texto: 'Este es mi comentario'
-  }
   return (
-    <div className="container mt-5">
-        <Comentario sujeto={sujeto} />
-        <Comentario sujeto={sujeto} />
-    </div>
+    <Router>
+      <div className="container mt-5">
+          <div className="btn-group">
+            <Link to="/" className="btn btn-dark">
+              Inicio
+            </Link>
+            <Link to="/contacto" className="btn btn-dark">
+              Contactenos
+            </Link>
+            <Link to="/nosotros" className="btn btn-dark">
+              Nosotros
+            </Link>
+          </div>
+          <hr />
+          <Switch>
+            <Route path="/contacto">
+              <Contacto />
+            </Route>
+            <Route path="/nosotros">
+              <Nosotros />
+            </Route>
+            <Route path="/">
+              <Inicio />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
